e850207d1ffb3b276be79fa4eaaddde6ad3bfbe4
FoxyGDFD <mic.panfil0v@gmail.com>
2024-03-14 21:58:30 +0300

    feat: add pain page draft
    	modified:   web/app/static/app/css/base.css
    	modified:   web/app/static/components/css/card.css
    	new file:   web/app/static/pages/css/main.css
        new file:   web/app/static/pages/img/app-store.png
    	new file:   web/app/static/pages/img/google-play.png
    	new file:   web/app/static/pages/img/main.png
    	new file:   web/app/static/pages/img/stores.png
    	new file:   web/app/static/pages/img/ul-arrow.svg
    	new file:   web/app/templates/pages/color.html
    	modified:   web/app/templates/pages/cut.html
    	new file:   web/app/templates/pages/main.html
    	modified:   web/app/urls.py
    	modified:   web/app/views.py

--- web/app/urls.py
+++ web/app/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('', MainView.as_view(), name='main'),
     path('carat/', CaratView.as_view(), name='carat'),
-    path('free-vygem/', FreeVygemView.as_view(), name='free-vygem'),
-    path('cut/', CutView.as_view(), name='cut'),
+    path('color/', ColorView.as_view(), name='color'),
     path('transparency/', TransparencyView.as_view(), name='transparency'),
+    path('cut/', CutView.as_view(), name='cut'),
+    path('free-vygem/', FreeVygemView.as_view(), name='free-vygem'),
 ]
--- web/app/views.py
+++ web/app/views.py
@@ -1,6 +1,15 @@
 from django.views import View
 from django.shortcuts import render
 
+
+class MainView(View):
+    @staticmethod
+    def get(request):
+        return render(request, 'pages/main.html', { 
+            'title': 'Vygem',
+            })
+
+
 class CaratView(View):
     @staticmethod
     def get(request):
@@ -54,3 +63,20 @@ class TransparencyView(View):
             'card': 'Transparency is understood as the ability of a solid substance to allow light rays to pass through to some degree. The transparency depends on the structure of the crystals, the presence of cracks, solid and gas-liquid inclusions, which hinder the passage of light through the stone. Transparency is visually determined by viewing the stone in transmitted light. Gemstones are classified by their transparency level as follows: \
                 <ul><li>Transparent: all colorless and lightly colored gemstones, through which objects are clearly visible when viewed through plates (3-5 mm thick);</li><li>Translucent: gemstones through which objects are somewhat visible but not clearly;</li><li>Translucent: gemstones through which objects are not clearly visible;</li><li>Opaque: gemstones through which no light passes through.</li></ul> \
                 The degree of transparency, namely the values ​​of the transparency coefficient and the absorption coefficient, can be determined quantitatively using spectrophotometers.' })
+
+
+class ColorView(View):
+    @staticmethod
+    def get(request):
+        return render(request, 'pages/color.html', { 
+            'title': 'Color',
+            'page_title': 'COLOR',
+            'card': "<p>Color is the most important characteristic when evaluating a gemstone.</p> \
+                <p>Our team has developed a methodology for determining the color of a gemstone based on computer and spectral color analysis. The essence of the methodology lies in determining the percentage of the base color hue and based on the data obtained, we propose to determine the color grading coefficient.</p> \
+                <p>Our application utilizes the following methodology to determine the value of a gemstone based on its color. We take 32 basic color hues used by the GIA. For each basic hue, we have compiled a table representing a 'plane' where the abscissa represents 'Saturation', determining how pure and neutral the color of the stone is, and the ordinate represents 'Brightness', determining the intensity of the color from light to dark. At each coordinate point in our color table, the color contains a certain amount of the base color hue in percentage. For example, for the color hue 'R (Red)', the first point 1/1 will contain almost 100% red color. And the second point 2/4 will contain approximately 72% red color. Therefore, the value of a stone with a color matching the second point will be the value of the first point multiplied by 0.72. However, this does not apply to the entire table. Once the amount of the base color crosses the 50% threshold and the base color starts to 'fade', we add an additional discount coefficient to the stone value.</p> \
+                <p>For example:<br> \
+                Vivid/Medium (6/5) will have a coefficient of 1.<br> \
+                Vivid/Light (6/3) will have a coefficient of 0.7<br> \
+                Moderately strong/Medium light (4/4) will have a coefficient of 0.65<br></p> \
+                <p>Using the example of the color table for the Red color hue presented below, the 'points of contact' between the VYGEM color methodology and the GIA color system are illustrated.</p>",
+            })
